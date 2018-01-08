c33e76d0aac030d129710246373bf0843652b280
Duncan Calvert <dcalvert@ihmc.us>
2018-01-08 10:28:50 -0600

    0.2.0 release.

---
+++ ihmc-ros2-library/src/test/java/us/ihmc/ros2/atlasMock/MockAtlasController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 Florida Institute for Human and Machine Cognition (IHMC)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package us.ihmc.ros2.atlasMock;
+
+import controller_msgs.msg.dds.RobotConfigurationData;
+import controller_msgs.msg.dds.RobotConfigurationDataPubSubType;
+import std_msgs.msg.dds.StringPubSubType;
+import us.ihmc.ros2.RosNode;
+import us.ihmc.ros2.RosPublisher;
+
+import java.io.IOException;
+
+public class MockAtlasController
+{
+   public static void main(String[] args) throws IOException, InterruptedException
+   {
+      RosNode node = new RosNode("MockAtlasController");
+      RosPublisher<RobotConfigurationData> publisher = node.createPublisher(new RobotConfigurationDataPubSubType(), "/robot_configuration_data");
+
+      for (int i = 0; true; i++)
+      {
+         RobotConfigurationData robotConfigurationData = new RobotConfigurationData();
+
+         robotConfigurationData.setTimestamp(i);
+         System.out.println("Publishing: " + i);
+         publisher.publish(robotConfigurationData);
+         Thread.sleep(1000);
+      }
+   }
+}
---
+++ ihmc-ros2-library/src/test/java/us/ihmc/ros2/atlasMock/MockNetworkProcessor.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017 Florida Institute for Human and Machine Cognition (IHMC)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package us.ihmc.ros2.atlasMock;
+
+import controller_msgs.msg.dds.RobotConfigurationData;
+import controller_msgs.msg.dds.RobotConfigurationDataPubSubType;
+import us.ihmc.pubsub.common.MatchingInfo;
+import us.ihmc.pubsub.subscriber.Subscriber;
+import us.ihmc.pubsub.subscriber.SubscriberListener;
+import us.ihmc.ros2.RosNode;
+
+import java.io.IOException;
+
+public class MockNetworkProcessor
+{
+   private static class Callback implements SubscriberListener
+   {
+      RobotConfigurationData robotConfigurationData = new RobotConfigurationData();
+
+      @Override
+      public void onNewDataMessage(Subscriber subscriber)
+      {
+         try
+         {
+            if (subscriber.takeNextData(robotConfigurationData, null))
+            {
+               System.out.println(robotConfigurationData.getTimestamp());
+            }
+         }
+         catch (IOException e)
+         {
+            e.printStackTrace();
+         }
+      }
+
+      @Override
+      public void onSubscriptionMatched(Subscriber subscriber, MatchingInfo info)
+      {
+
+      }
+   }
+
+   public static void main(String[] args) throws IOException, InterruptedException
+   {
+      RosNode node = new RosNode("MockNetworkProcessor");
+      node.createSubscription(new RobotConfigurationDataPubSubType(), new Callback(), "/robot_configuration_data");
+      
+      Thread.currentThread().join();
+   }
+}
