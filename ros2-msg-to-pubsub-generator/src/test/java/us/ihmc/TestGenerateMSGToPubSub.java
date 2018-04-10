555a39ced1c15244ff0047ce5ce5d5fda16c191f
Duncan Calvert <dcalvert@ihmc.us>
2018-04-10 15:41:32 -0500

    Delete old generated messages.

--- ros2-msg-to-pubsub-generator/src/test/java/us/ihmc/TestGenerateMSGToPubSub.java
+++ ros2-msg-to-pubsub-generator/src/test/java/us/ihmc/TestGenerateMSGToPubSub.java
@@ -1,5 +1,6 @@
 package us.ihmc;
 
+import us.ihmc.commons.nio.FileTools;
 import us.ihmc.ros2.rosidl.RosInterfaceGenerator;
 
 import java.io.IOException;
@@ -9,6 +10,9 @@ public class TestGenerateMSGToPubSub
 {
    public static void main(String[] args) throws IOException
    {
+      FileTools.deleteQuietly(Paths.get("generated-java"));
+      FileTools.deleteQuietly(Paths.get("generated-idl"));
+
       RosInterfaceGenerator generator = new RosInterfaceGenerator();
 
       generator.addPackageRoot(Paths.get("ros_msgs"));
