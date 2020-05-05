a5fa004b20b8e0be1720d02a0a999451446d69bb
Duncan Calvert <dcalvert@ihmc.us>
2020-05-05 16:14:30 -0500

    Move ROS2Input and ROS2Callback down to IHMC ROS 2 Library.

---
+++ ihmc-ros2-library/src/main/java/us/ihmc/ros2/ROS2Callback.java
@@ -0,0 +1,63 @@
+package us.ihmc.ros2;
+
+import us.ihmc.commons.exception.DefaultExceptionHandler;
+import us.ihmc.commons.exception.ExceptionTools;
+import us.ihmc.log.LogTools;
+import us.ihmc.pubsub.subscriber.Subscriber;
+
+import java.util.function.Consumer;
+
+/**
+ * Callback listener to non-null reception of a message on a ROS 2 topic.
+ *
+ * @param <T> messageType
+ */
+public class ROS2Callback<T>
+{
+   private final Consumer<T> messageCallback;
+   private Ros2Subscription<T> subscription;
+   private volatile boolean enabled = true;
+
+   public ROS2Callback(Ros2NodeInterface ros2Node, Class<T> messageType, ROS2TopicName topicName, Consumer<T> messageCallback)
+   {
+      this(ros2Node, messageType, topicName.type(messageType).toString(), messageCallback);
+   }
+
+   public ROS2Callback(Ros2NodeInterface ros2Node, Class<T> messageType, String topicName, Consumer<T> messageCallback)
+   {
+      this.messageCallback = messageCallback;
+      ExceptionTools.handle(() ->
+      {
+         subscription = ros2Node.createSubscription(ROS2TopicNameTools.newMessageTopicDataTypeInstance(messageType), this::nullOmissionCallback, topicName);
+      }, DefaultExceptionHandler.RUNTIME_EXCEPTION);
+   }
+
+   private void nullOmissionCallback(Subscriber<T> subscriber)
+   {
+      if (enabled)
+      {
+         T incomingData = subscriber.takeNextData();
+         if (incomingData != null)
+         {
+            messageCallback.accept(incomingData);
+         }
+         else
+         {
+            LogTools.warn("Received null from takeNextData()");
+         }
+      }
+   }
+
+   public void setEnabled(boolean enabled)
+   {
+      this.enabled = enabled;
+   }
+
+   public void destroy()
+   {
+      if (subscription != null)
+      {
+         subscription.remove();
+      }
+   }
+}
