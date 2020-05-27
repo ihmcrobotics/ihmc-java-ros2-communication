51eac287d993a7fbb0a9f02043949b7fbb11f703
Duncan Calvert <dcalvert@ihmc.us>
2020-05-26 23:04:32 -0500

    Add javadoc

--- ihmc-ros2-library/src/main/java/us/ihmc/ros2/ROS2Topic.java
+++ ihmc-ros2-library/src/main/java/us/ihmc/ros2/ROS2Topic.java
@@ -6,6 +6,46 @@ import java.util.function.Function;
 import static us.ihmc.ros2.ROS2TopicNameTools.messageTypeToTopicNamePart;
 import static us.ihmc.ros2.ROS2TopicNameTools.processTopicNamePart;
 
+/**
+ * <p>
+ * A ROS 2 topic, represented as a String builder with a type. It is designed to be an immutable class with
+ * equals, hashCode, and toString support.
+ * </p>
+ * <p>The immutable methods start with "with".</p>
+ * <p>It considers a ROS 2 topic name to be 5 string parts put together:</p>
+ * <ul>
+ *    <li>prefix</li>
+ *    <li>robot name</li>
+ *    <li>module name</li>
+ *    <li>io qualifier</li>
+ *    <li>suffix</li>
+ * </ul>
+ * <p>
+ * The toString() method and the getName() methods evaluate to the topic name i.e. /ihmc/controller/output/status
+ * </p>
+ * <p>
+ * It also is parameterized by type. This type is used to enforce topic type safety and
+ * increase the usefulness of helper methods.
+ *
+ * Before the message type is known, for example the IHMC_ROOT topic in ROS2Tools,
+ * the user would use the ? as ROS2Topic<?> to say the type has not yet been decided.
+ * </p>
+ * <p>
+ * The standard usage of this class would be:
+ * <pre>
+ *    // evaluates to "/ihmc/atlas/rea/example_type/one"
+ *    ROS2Topic topicName = new ROS2Topic().withPrefix("ihmc").withRobot("atlas").withModule("rea")
+ *                                            .withType(ExampleTypeMessage.class).withNaming(typeName -> typeName + "/one");
+ *
+ *    // use of immutable property
+ *    // evaluates to "/ihmc"
+ *    ROS2Topic ihmcPrefixed = new ROS2Topic().withPrefix("ihmc");
+ *    // evaluates to "/ihmc/hello"
+ *    ROS2Topic hello = ihmcPrefixed.withModule("hello");
+ * </pre>
+ *
+ * @param <T>
+ */
 public class ROS2Topic<T>
 {
    public static final String INPUT = "input";
