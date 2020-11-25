7ee6c20b1a64f568bec46205fd569d6bc856c1f8
Duncan Calvert <dcalvert@ihmc.us>
2020-11-24 23:35:26 -0600

    :arrow_up: ihmc-ci 7.1

--- ros2-common-interfaces/build.gradle.kts
+++ ros2-common-interfaces/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
 plugins {
    id("us.ihmc.ihmc-build") version "0.22.0"
-   id("us.ihmc.ihmc-ci") version "7.0"
+   id("us.ihmc.ihmc-ci") version "7.1"
    id("us.ihmc.ihmc-cd") version "1.16"
    id("org.ajoberstar.grgit") version "3.1.1"
 }
