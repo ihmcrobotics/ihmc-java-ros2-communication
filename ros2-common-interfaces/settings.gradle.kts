95506710d818f93b13a085703248ce28527f0b30
Duncan Calvert <dcalvert@ihmc.us>
2020-12-18 17:29:23 -0600

    :arrow_up: ihmc-build 0.23.5 Add jitpack to snapshot mode to fix Bamboo.

--- ros2-common-interfaces/settings.gradle.kts
+++ ros2-common-interfaces/settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
       maven(url = "https://dl.bintray.com/ihmcrobotics/maven-release")
    }
    plugins {
-      id("us.ihmc.ihmc-build") version "0.23.4"
+      id("us.ihmc.ihmc-build") version "0.23.5"
    }
 }
 
@@ -14,7 +14,7 @@ buildscript {
       mavenLocal()
    }
    dependencies {
-      classpath("us.ihmc:ihmc-build:0.23.4")
+      classpath("us.ihmc:ihmc-build:0.23.5")
    }
 }
 
