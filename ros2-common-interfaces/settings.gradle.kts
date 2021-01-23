9fa4621a764b9af904d9ce6557e56f95e018d53f
Duncan Calvert <dcalvert@ihmc.us>
2021-01-23 01:31:09 -0600

    :arrow_up: ihmc-build 0.23.7

--- ros2-common-interfaces/settings.gradle.kts
+++ ros2-common-interfaces/settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
       maven(url = "https://dl.bintray.com/ihmcrobotics/maven-release")
    }
    plugins {
-      id("us.ihmc.ihmc-build") version "0.23.6"
+      id("us.ihmc.ihmc-build") version "0.23.7"
    }
 }
 
@@ -14,7 +14,7 @@ buildscript {
       mavenLocal()
    }
    dependencies {
-      classpath("us.ihmc:ihmc-build:0.23.6")
+      classpath("us.ihmc:ihmc-build:0.23.7")
    }
 }
 
