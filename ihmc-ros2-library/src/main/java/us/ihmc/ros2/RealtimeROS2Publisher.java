f7842095ab950c6dafa2a9450663500faaaa147d
Jesper Smith <jesper@halodi.com>
2021-10-26 16:25:53 +0200

    Created backwards compatibility for the RealtimeROS2Publisher in a slightly hack-ish way

---
+++ ihmc-ros2-library/src/main/java/us/ihmc/ros2/RealtimeROS2Publisher.java
@@ -0,0 +1,14 @@
+package us.ihmc.ros2;
+
+/**
+ * Helper interface for backwards compatibility
+ * 
+ * @author Jesper Smith
+ *
+ * @param <T>
+ */
+@Deprecated
+public interface RealtimeROS2Publisher<T> extends ROS2PublisherBasics<T>
+{
+
+}
