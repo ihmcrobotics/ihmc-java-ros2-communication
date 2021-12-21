f337de64572c30b2ea7e0ba1b90a154dadf6e890
Duncan Calvert <dcalvert@ihmc.us>
2021-12-20 20:31:00 -0600

    :arrow_up: ihmc-commons 0.30.5 upgraded dependencies including log4j fix

--- ihmc-ros2-library/build.gradle.kts
+++ ihmc-ros2-library/build.gradle.kts
@@ -14,11 +14,11 @@ ihmc {
 mainDependencies {
    api("us.ihmc:ihmc-pub-sub:0.16.2")
    api("us.ihmc:ihmc-realtime:1.4.0")
-   api("us.ihmc:ihmc-commons:0.30.4")
+   api("us.ihmc:ihmc-commons:0.30.5")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-test:0.30.4")
+   api("us.ihmc:ihmc-commons-test:0.30.5")
    api("us.ihmc:ros2-common-interfaces:source")
    api("us.ihmc:ros2-msg-to-pubsub-generator-test:source")
    api("com.google.guava:guava:18.0")
