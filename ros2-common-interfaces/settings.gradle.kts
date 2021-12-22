682283d95cc48b90aa88c41d938f4c58ff28785e
Duncan Calvert <dcalvert@ihmc.us>
2021-12-21 23:45:23 -0600

    :arrow_up: ihmc-build 0.27.1 Require Gradle 7 and support Sonatype Nexus.

--- ros2-common-interfaces/settings.gradle.kts
+++ ros2-common-interfaces/settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
       gradlePluginPortal()
    }
    plugins {
-      id("us.ihmc.ihmc-build") version "0.26.4"
+      id("us.ihmc.ihmc-build") version "0.27.1"
    }
 }
 
@@ -13,7 +13,7 @@ buildscript {
       mavenLocal()
    }
    dependencies {
-      classpath("us.ihmc:ihmc-build:0.26.4")
+      classpath("us.ihmc:ihmc-build:0.27.1")
    }
 }
 
