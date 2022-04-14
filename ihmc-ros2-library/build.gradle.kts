660a56e6fe3ff8eea9bf1b4aa73617a52ac37f69
Sylvain Bertrand <sbertrand@ihmc.org>
2022-04-14 17:08:00 -0500

    :arrow_up: ihmc-realtime 1.5.0

--- ihmc-ros2-library/build.gradle.kts
+++ ihmc-ros2-library/build.gradle.kts
@@ -13,7 +13,7 @@ ihmc {
 
 mainDependencies {
    api("us.ihmc:ihmc-pub-sub:0.16.2")
-   api("us.ihmc:ihmc-realtime:1.4.0")
+   api("us.ihmc:ihmc-realtime:1.5.0")
    api("us.ihmc:ihmc-commons:0.30.4")
 }
 
