d76659c48a7fdb1f3c340332c776cfc22bbf3800
Duncan Calvert <dcalvert@ihmc.us>
2022-07-01 13:46:26 -0500

    :bookmark: 0.21.1

--- ihmc-ros2-library/build.gradle.kts
+++ ihmc-ros2-library/build.gradle.kts
@@ -12,7 +12,7 @@ ihmc {
 }
 
 mainDependencies {
-   api("us.ihmc:ihmc-pub-sub:0.16.2")
+   api("us.ihmc:ihmc-pub-sub:0.16.3")
    api("us.ihmc:ihmc-realtime:1.4.0")
    api("us.ihmc:ihmc-commons:0.31.0")
 }
