36bd14c4399c82cdb7a64e2defc9e7de6edd0d60
Duncan Calvert <dcalvert@ihmc.us>
2023-02-13 11:47:32 -0600

    :arrow_up: ihmc-build 0.28.7

--- ros2-common-interfaces/settings.gradle.kts
+++ ros2-common-interfaces/settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
       gradlePluginPortal()
    }
    plugins {
-      id("us.ihmc.ihmc-build") version "0.28.6"
+      id("us.ihmc.ihmc-build") version "0.28.7"
    }
 }
 
@@ -13,7 +13,7 @@ buildscript {
       mavenLocal()
    }
    dependencies {
-      classpath("us.ihmc:ihmc-build:0.28.6")
+      classpath("us.ihmc:ihmc-build:0.28.7")
    }
 }
 
