2cd2ffd9e57c3e1aa6dfacd3bfa1c52bd9e9ed11
Dexton Anderson <danderson@ihmc.org>
2023-05-08 12:50:32 -0500

    javadoc

--- ihmc-ros2-library/src/main/java/us/ihmc/ros2/NewMessageListener.java
+++ ihmc-ros2-library/src/main/java/us/ihmc/ros2/NewMessageListener.java
@@ -22,6 +22,11 @@ import us.ihmc.pubsub.subscriber.SubscriberListener;
 @FunctionalInterface
 public interface NewMessageListener<T> extends SubscriberListener<T>
 {
+   /**
+    * Virtual function to be implemented by the user containing the actions to be performed when a new data message is received.
+    *
+    * @param subscriber
+    */
    void onNewDataMessage(Subscriber<T> subscriber);
 
    default void onSubscriptionMatched(Subscriber<T> subscriber, MatchingInfo info)
